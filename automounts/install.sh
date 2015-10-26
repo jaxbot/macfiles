1ea47db76ee2cb65f96302c3a31b6eb8d02348f8
Jonathan Warner <jaxbot@gmail.com>
2015-10-25 22:18:19 -0400

    Improve automount setup

--- automounts/install.sh
+++ automounts/install.sh
@@ -1,3 +1,6 @@
-mkdir /etc/automounts
+sudo mkdir /etc/automounts
 sudo cp applepi /etc/automounts/
-sudo cat auto_master >> /etc/auto_master
+#sudo cat auto_master | sudo tee -a /etc/auto_master > /dev/null
+sudo automounts -vc
+echo "192.168.1.100 applepi" | sudo tee -a /etc/hosts
+open /nfs/seagate
